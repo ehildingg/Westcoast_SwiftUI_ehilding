8ff830c9bf4c663ac17efb32c53cac217c69c87e
ehilding <erik.hildingsson@gmail.com>
2022-02-20 11:42:55 +0100

    Initial Commit

---
+++ Westcoast_SwiftUI_ehilding/Westcoast_SwiftUI_ehildingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Westcoast_SwiftUI_ehildingApp.swift
+//  Westcoast_SwiftUI_ehilding
+//
+//  Created by Erik Hildingsson on 2022-02-20.
+//
+
+import SwiftUI
+
+@main
+struct Westcoast_SwiftUI_ehildingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
